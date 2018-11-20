033e07feb5b8b9da5546b77024af97761f592d6c
SimasDei <32547810+SimasDei@users.noreply.github.com>
2018-11-20 15:56:45 +0200

    event handlers

--- src/App.js
+++ src/App.js
@@ -1,28 +1,128 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      value: 'Type Something',
+      style: {
+        backgroundColor: 'orangered',
+        padding: '16px',
+        cursor: 'pointer',
+        color: '#fff'
+      }
+    }
+  }
+
+
+
+  changeHandler(event, value) {
+    console.log(event);
+    this.setState({ 'value': event.target.value })
+
+  }
+
+
+
+  clickHandler(event) {
+    console.log(event);
+    this.setState({
+      style: {
+        backgroundColor: 'salmon',
+        padding: '16px',
+        transform: 'scale(2)',
+        color: '#fff',
+        fontSize: '20px'
+      }
+    })
+  }
+
+  mouseOverHandler(event) {
+    this.setState({
+      style: {
+        backgroundColor: 'salmon',
+        padding: '16px',
+        color: '#fff',
+        cursor: 'pointer',
+        transform: 'scale(2)',
+        marginBottom: '26px',
+        fontSize: '20px',
+        transition: 'all .5s'
+      }
+    })
+  }
+
+  mouseLeaveHandler(event) {
+    this.setState({
+      style: {
+        backgroundColor: 'orangered',
+        padding: '16px',
+        transition: 'all .5s'
+      }
+    })
+  }
+
+
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <br></br>
+        <hr></hr>
+        <br></br>
+
+        <ShowMany count={3} />
+
+        <br></br>
+        <hr></hr>
+        <br></br>
+
+        {getTestArray(5).map(element => {
+          return (<div>{element}</div>)
+        })}
+
+        <br></br>
+        <hr></hr>
+        <br></br>
+
+        <div>
+          <input
+            onClick={this.clickHandler.bind(this)}
+            onChange={this.changeHandler.bind(this)}
+            onMouseOver={this.mouseOverHandler.bind(this)}
+            onMouseLeave={this.mouseLeaveHandler.bind(this)}
+            value={this.state.value}
+            style={this.state.style} />
+        </div>
+
+        <div>
+          {this.state.value}
+        </div>
+
       </div>
     );
   }
+}
+
+// Tasks
+function ShowMany(params) {
+  let rows = [];
+  for (let i = 0; i < params.count; i++) {
+    rows.push(<div>Labas {i + 1}</div>)
+  }
+  return rows;
+}
+
+function getTestArray(params) {
+  let rows = [];
+  for (let i = 0; i < params; i++) {
+    rows.push(<div>{'Labas' + (i + 1)}</div>)
+  }
+  return rows;
 }
 
 export default App;
